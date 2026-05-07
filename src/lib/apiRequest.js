c41551b92afbe28501a032c6ca1242149d7e3b37
04-AKILESH <akileshnagarajan123@gmail.com>
2026-05-07 17:17:55 +0530

    cache

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -1,20 +1,37 @@
-const BASE = process.env.NEXT_PUBLIC_API_BASE_URL || "https://api.srimaniyainstitute.in";
+const BASE =
+	process.env.NEXT_PUBLIC_API_BASE_URL ||
+	"https://api.srimaniyainstitute.in";
 
 export const apiRequest = async ({
 	endpoint,
 	method = "GET",
 	body = null,
 	headers = {},
-	cache = "no-store",
+	cache = "force-cache",
+	revalidate = 60,
 	isFormData = false,
 }) => {
+	const isGetRequest = method === "GET";
+
 	const response = await fetch(`${BASE}${endpoint}`, {
 		method,
+
 		headers: {
-			...(isFormData ? {} : { "Content-Type": "application/json" }),
+			...(isFormData
+				? {}
+				: { "Content-Type": "application/json" }),
 			...headers,
 		},
-		cache,
+
+		...(isGetRequest
+			? {
+					cache,
+					next: { revalidate },
+			  }
+			: {
+					cache: "no-store",
+			  }),
+
 		...(body && {
 			body: isFormData ? body : JSON.stringify(body),
 		}),
